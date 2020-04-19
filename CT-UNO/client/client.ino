c1d64b43b499d3b179ef34b22f61b983dee4a9ca
Kittikom Sangrit <sangrit@Sangrits-MacBook-Pro.local>
2020-04-19 23:24:43 +0700

    add rpi node-red

---
+++ CT-UNO/client/client.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <ArduinoJson.h>
+
+#define INTERVAL 1000
+#define SENSOR_ID "CT-UNO-1"
+
+unsigned long time;
+
+RH_RF95 rf95;
+
+int pin_temperature = A0;
+int pin_moisture = A3;
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial) ; // Wait for serial port to be available
+  if (!rf95.init())
+    Serial.println("init failed");
+}
+
+void loop()
+{
+  time = millis();
+  uint8_t payload[128];
+  DynamicJsonDocument json(sizeof(payload));
+
+  json["sensor node"]      = SENSOR_ID;
+  json["temperature"] = analogRead(pin_temperature);
+  json["soil moisture"]    = analogRead(pin_moisture);
+    
+  serializeJson(json, (unsigned char*)payload, sizeof(payload));
+  
+  rf95.send(payload, sizeof(payload));
+  rf95.waitPacketSent();
+
+  Serial.println((char*)payload);
+  delay(INTERVAL*random(1,10));
+  //Serial.println(millis()-time);
+}
---
+++ Raspberry Pi/main.py
@@ -0,0 +1,34 @@
+import json
+import serial
+import paho.mqtt.client as mqtt
+
+NETPIE_ID = 'b4682d9c-c282-4bad-8d02-5ad6997b5cb9'
+NETPIE_TOKEN = 'tnrutv5Dqv2J9uCJUxKUUp5svd66iBUv'
+
+TOPICS_SHADOW = {
+    "update": "@shadow/data/update"
+}
+
+TOPICS_MSG = {
+    "feeds": "@msg/landslide/feeds/"
+}
+
+client = mqtt.Client(client_id=NETPIE_ID)
+
+if __name__ == "__main__":
+
+#    client.on_connect = on_connect
+#    client.on_message = on_message
+    client.username_pw_set(NETPIE_TOKEN)
+    client.connect("mqtt.netpie.io", 1883, 60)
+    client.loop_start()
+
+    with serial.Serial('/dev/tty.usbserial-D3071WWY', 9600) as ser:
+        while(True) :
+            data = ser.readline().decode('utf-8')
+            data = json.loads(data)
+            payload = {"data": data}
+            payload = json.dumps(payload)
+            client.publish(TOPICS_MSG["feeds"], payload=payload)
+            client.publish(TOPICS_SHADOW['update'], payload=payload, qos=0, retain=False)
+            print(payload)
