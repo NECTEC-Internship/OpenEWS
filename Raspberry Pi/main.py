09828975dd0a81a2a77e1a3f32f19f5879ed8df1
Kittikom Sangrit <sangrit@Sangrits-MacBook-Pro.local>
2020-04-20 12:24:08 +0700

    in rpi add def main

--- Raspberry Pi/main.py
+++ Raspberry Pi/main.py
@@ -2,6 +2,8 @@ import json
 import serial
 import paho.mqtt.client as mqtt
 
+USB_DEVICE = '/dev/ttyUSB_CT-UNO'
+
 NETPIE_ID = 'b4682d9c-c282-4bad-8d02-5ad6997b5cb9'
 NETPIE_TOKEN = 'tnrutv5Dqv2J9uCJUxKUUp5svd66iBUv'
 
@@ -15,20 +17,29 @@ TOPICS_MSG = {
 
 client = mqtt.Client(client_id=NETPIE_ID)
 
-if __name__ == "__main__":
 
+def main():
 #    client.on_connect = on_connect
 #    client.on_message = on_message
     client.username_pw_set(NETPIE_TOKEN)
     client.connect("mqtt.netpie.io", 1883, 60)
     client.loop_start()
-
     with serial.Serial('/dev/tty.usbserial-D3071WWY', 9600) as ser:
         while(True) :
-            data = ser.readline().decode('utf-8')
-            data = json.loads(data)
+            try:
+                data = ser.readline().decode('utf-8')
+                data = json.loads(data)
+            except json.decoder.JSONDecodeError, e:
+                print(e)
+                continue
             payload = {"data": data}
             payload = json.dumps(payload)
             client.publish(TOPICS_MSG["feeds"], payload=payload)
             client.publish(TOPICS_SHADOW['update'], payload=payload, qos=0, retain=False)
             print(payload)
+        #while
+    #with
+#def main
+
+if __name__ == "__main__":
+    main()
